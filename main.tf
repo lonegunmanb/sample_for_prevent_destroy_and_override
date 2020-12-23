2ed14ea0778d793842ac928eeffaa72a4a588184
lonegunman <lonegunmanb@hotmail.com>
2020-12-23 11:59:26 +0800

    remove provider

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-provider "ucloud" {
-  public_key  = var.public_key
-  private_key = var.private_key
-  project_id  = var.project_id
-  region      = var.region
-}
-
 data "ucloud_images" "centos" {
   name_regex        = "^CentOS 7"
   availability_zone = var.az
