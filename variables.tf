7966d742cb90d5be5dde0312a3243a47da6446ff
lonegunman <lonegunmanb@hotmail.com>
2020-12-23 12:09:55 +0800

    remove unused variables

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-variable public_key {
-  type = string
-}
-variable private_key {
-  type = string
-}
-variable project_id {
-  type = string
-}
-variable region {
-  type = string
-}
 variable "az" {
   type = string
 }
